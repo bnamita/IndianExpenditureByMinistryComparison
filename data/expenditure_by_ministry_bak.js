bac44c9f33645736360b579bec712ab2fb064d15
nbhatawdekar <autocad7>
2016-03-02 22:00:44 +0530

    2016-17

---
+++ data/expenditure_by_ministry_bak.js
@@ -0,0 +1,24 @@
+var exp_data = [
+  ["Department","Expenditure (2015-16)","Expenditure (2014-15)"],
+  ["Finance",679960.98,640404.18],
+  ["Defence",310079.60,285202.87],
+  ["Consumer Affairs, Food and Public Distribution",125473.77,115952.63],
+  ["Chemicals and Fertilizers",73562.00,73618.55],
+  ["Rural Development",73332.58,83852.46],
+  ["Human Resource Development",69074.76,82771.1],
+  ["Home Affairs",68444.10,65745.28],
+  ["Road Transport and Highways",45751.65,34345.2],
+  ["Railways",40000.00,30100],
+  ["Health and Family Welfare",32068.17,39237.82],
+  ["Petroleum and Natural Gas",30125.55,63543],
+  ["Agriculture and Farmers Welfare",24909.78,31062.94],
+  ["Communications and Information Technology",23243.30,26169.92],
+  ["Urban Development",19216.87,20009.46],
+  ["External Affairs",14966.83,14730.39]
+];
+
+
+
+
+
+
